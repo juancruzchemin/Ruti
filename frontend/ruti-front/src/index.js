9a3268792bb30c2902de0ef558f17d8ade05ca8f
juancruzchemin <juancruzcheminv@gmail.com>
2024-11-24 02:12:19 +0100

    Updating index.js

--- frontend/ruti-front/src/index.js
+++ frontend/ruti-front/src/index.js
@@ -1,17 +1,18 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
+import NotFound from './NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
 );
+
